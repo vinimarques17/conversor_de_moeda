d5204cd5a91885f90de9575aa30ce974fcfb3b72
vinimarques17 <vinicius.marx17@gmail.com>
2024-09-24 22:55:39 -0300

    deleted archive Objeto_.py

--- Objeto_.py
+++
@@ -1,9 +0,0 @@
-pessoa = {
-  "nome": "Vinicius",
-  "sobrenome": "Mantovani",
-  "idade": "24",
-  "profissao": "Futuro garoto de programa",
-}
-print (pessoa["nome"]+" "+pessoa["sobrenome"])
-print ("Idade Atual"+": "+pessoa["idade"])
-print (pessoa["profissao"])
