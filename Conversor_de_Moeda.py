b38badd9b4f335726d3e1b7c8a92fa83156a6271
vinimarques17 <vinicius.marx17@gmail.com>
2024-09-24 23:40:57 -0300

    feat: add the conditional for choose the coin

--- Conversor_de_Moeda.py
+++ Conversor_de_Moeda.py
@@ -3,6 +3,19 @@ dolar = float(real) * 5.45
 euro = float(real) * 6.10
 peso_argentino = float(real) * 177.08
 
-print(f'A cotacão do Dólar hoje é {dolar:.2f}.')
-print(f'A cotacão do Euro hoje é {euro:.2f}.')
-print(f'A cotacão do Peso Argetino hoje é {peso_argentino:.2f}.')
+choose = input('Qual moeda irá converter? ')
+
+if choose == 'Dolar':
+    print(f'A cotacão do Dólar hoje é {dolar:.2f}.')
+elif choose == 'Euro':
+    print(f'A cotacão do Euro hoje é {euro:.2f}.')
+elif choose == 'Peso':
+    print(f'A cotacão do Peso Argetino hoje é {peso_argentino:.2f}.')
+else:
+    print('Digite uma moeda válida')
+
+
+
+# print(f'A cotacão do Dólar hoje é {dolar:.2f}.')
+# print(f'A cotacão do Euro hoje é {euro:.2f}.')
+# print(f'A cotacão do Peso Argetino hoje é {peso_argentino:.2f}.')
