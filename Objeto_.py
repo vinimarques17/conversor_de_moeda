d9a81e8b53e7eac6775c9fe782add7d18fa4cb18
vinimarques17 <vinicius.marx17@gmail.com>
2024-09-24 22:49:54 -0300

    first commit

---
+++ Objeto_.py
@@ -0,0 +1,9 @@
+pessoa = {
+  "nome": "Vinicius",
+  "sobrenome": "Mantovani",
+  "idade": "24",
+  "profissao": "Futuro garoto de programa",
+}
+print (pessoa["nome"]+" "+pessoa["sobrenome"])
+print ("Idade Atual"+": "+pessoa["idade"])
+print (pessoa["profissao"])
