fecfd26e87a043c397c01b3c9a441166bf032e9b
vinimarques17 <vinicius.marx17@gmail.com>
2024-09-26 23:29:11 -0300

    feat: Modify the order code (Cotation for all coins) and add :.2f limited

--- Conversor_de_Moeda.py
+++ Conversor_de_Moeda.py
@@ -1,15 +1,39 @@
-real = input('Qual o valor a converter? ')
-dolar = float(real) * 5.45
-euro = float(real) * 6.10
-peso_argentino = float(real) * 177.08
+# Declarando variaveis do escopo
+dolar = 5.45
+euro = 6.10
+peso = 177.08
 
+# tornando um função para facilitar na manutenção do código
+def converter(value: float, moeda: str):  
+    if (moeda == 'dolar'):
+        return value * dolar
+    elif (moeda == 'euro'):
+        return value * euro
+    elif (moeda == 'peso'):
+        return value * peso
+    else:
+        return False
+
+# Nova feature para o usuário
+def compareCoins(value: float):
+    compare_dolar = value * dolar
+    compare_euro = value * euro
+    compare_peso = value * peso
+    return print(f'Dolar: {compare_dolar:.2f} | Euro: {compare_euro:.2f} | Pesos argentinos: {compare_peso:.2f}')
+
+# inputs
+real = input('Qual o valor a converter? ')
 choose = input('Qual moeda irá converter? ')
 
-if (choose == 'Dolar' or choose == 'dolar'):
-    print(f'A cotacão do Dólar hoje é {dolar:.2f}.')
-elif (choose == 'Euro' or choose == 'euro'):
-    print(f'A cotacão do Euro hoje é {euro:.2f}.')
-elif (choose == 'Peso' or choose == 'peso'):
-    print(f'A cotacão do Peso Argetino hoje é {peso_argentino:.2f}.')
+# Transformando o texto em letras minusculas
+choose = choose.lower()
+
+# Validando os dados recebidos e printando
+if converter(float(real), choose):
+    print(f'A cotação em {choose} resultou em: $ {converter(float(real), choose):.2f}')
+# Print na cotação de todas moedas
+    print('A cotação para cada moeda:')
+    compareCoins(float(real))
 else:
-    print('Digite uma moeda válida')
+    print('Não foi possível fazer a conversão, moeda inválida.')
+
