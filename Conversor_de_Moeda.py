0ed0520867cf624261c65643072d6d1cd3d5970a
vinimarques17 <vinicius.marx17@gmail.com>
2024-09-26 22:54:22 -0300

    feat: Add the variables name coins

--- Conversor_de_Moeda.py
+++ Conversor_de_Moeda.py
@@ -5,11 +5,11 @@ peso_argentino = float(real) * 177.08
 
 choose = input('Qual moeda irá converter? ')
 
-if choose == 'Dolar':
+if (choose == 'Dolar' or choose == 'dolar'):
     print(f'A cotacão do Dólar hoje é {dolar:.2f}.')
-elif choose == 'Euro':
+elif (choose == 'Euro' or choose == 'euro'):
     print(f'A cotacão do Euro hoje é {euro:.2f}.')
-elif choose == 'Peso':
+elif (choose == 'Peso' or choose == 'peso'):
     print(f'A cotacão do Peso Argetino hoje é {peso_argentino:.2f}.')
 else:
     print('Digite uma moeda válida')
